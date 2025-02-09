5236c6ad759583e0d8d881ff316c95948459a9cf
dustinburda <dustin99wii@gmail.com>
2025-02-08 21:18:55 -0500

    created directories, tuple class

---
+++ include/Tuple.h
@@ -0,0 +1,14 @@
+//
+// Created by Dustin on 2/8/25.
+//
+
+#ifndef SMALLDB_TUPLE_H
+#define SMALLDB_TUPLE_H
+
+
+class Tuple {
+
+};
+
+
+#endif //SMALLDB_TUPLE_H
---
+++ src/Tuple.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dustin on 2/8/25.
+//
+
+#include "../include/Tuple.h"
